7c6dc9c8cfc86e7deb3eec39a692852549b191d4
Claudio <rickandmxnster@gmail.com>
2024-09-12 22:06:13 -0300

    setup do ambiente de desenvolvimento

---
+++ hello.js
@@ -0,0 +1,29 @@
+┏━━━━━━━━━━━━━━━━━━━━━━━━━┓
+/**
+ * Hello World
+ * @author Claudio Silva
+ */
+
+console.log("Hello World")
+
+┗━━━━━━━━━━━━━━━━━━━━━━━━━┛
+______________________________________________________
+Node - Plataforma que permite a execução do JavaScript
+fora do navegador (browser)
+https://nodejs.org/
+
+Comandos básicos:
+node -v (versão do node)
+
+Gerenciador de pacote para o node:
+npm está relacionado a pasta de projeto ou seja os
+pacotes são instalados na raiz do projeto e não no
+sistema operacional
+
+npm -v (versão do npm)
+npm ls (listar pacotes instalados)
+npm init (iniciar um novo projeto JavaScript)
+    |_____ package.json
+
+json (JavaScript Object Notation)
+
