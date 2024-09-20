d7712ed02ce872db8331734a38645736c5f56b53
Claudio <rickandmxnster@gmail.com>
2024-09-19 22:13:32 -0300

    Experiência importante

---
+++ Exercícios/academia.js
@@ -0,0 +1,29 @@
+/**
+ * Ficha de um aluno de academia
+ * @author Claudio Silva
+ */
+
+// variáveis
+let nome
+let idade
+let altura
+let peso
+let vip
+
+console.clear()
+
+// entrada de dados
+nome  = "Claudio Silva"
+idade = 24
+altura = 1.90
+peso = 72
+vip = true
+
+// saida
+console.log("Ficha do aluno")
+console.log("____________________")
+console.log(`Nome: ${nome}`)
+console.log(`idade: ${idade}`)
+console.log(`altura: ${altura}`)
+console.log(`peso: ${peso}`)
+console.log(`vip: ${vip}`)
