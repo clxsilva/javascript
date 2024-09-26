6413251ce883fa384a1209d4a63941550c4d483a
Claudio <rickandmxnster@gmail.com>
2024-09-26 19:53:42 -0300

    adições

--- Fundamentos/sintaxe1.js
+++ Fundamentos/sintaxe1.js
@@ -9,11 +9,32 @@ console.clear()
 // JS - Sentença e ";" opcional
 console.log("Hello World")
 
+let nome = "Claudio Silva"
+let idade = 19
+// o ponto é usado para casas decimais
+let altura = 1.90
+// typeof() é usado para identificar o tipo de
+// variável
+console.log(typeof(nome))
+console.log(typeof(idade))
+console.log(typeof(altura))
+
+// identificando variáveis inteiras e não inteiras
+console.log(Number.isInteger(idade))
+console.log(Number.isInteger(altura))
+
 // concatenação (unir textos e variáveis)
-let nome = "Dean Winchester"
 console.log(`Nome: ${nome}`)
 console.log("Nome: " + nome) // não recomendado
 
 // arredondamento (casas decimais)
 let media = 5.789
 console.log(`Média: ${media.toFixed(1)}`)
+
+// Particularidades do JavaScript
+console.log(10 / 0)
+console.log(0.5 + 0.2)
+console.log(0.1 + 0.2)
+console.log("3" + 2)
+console.log("3" - 2)
+console.log("3x" - 2)
