acdbf5a36ff5927db516168789bda802aaa6958a
Claudio <rickandmxnster@gmail.com>
2024-09-26 22:20:42 -0300

    fundamentos

---
+++ Fundamentos/biblioteca.js
@@ -0,0 +1,11 @@
+/**
+ *  Exemplo de uso de pacotes externos ("bibliotecas")
+ * @author Claudio Silva
+ */
+
+// importar o pacote
+const colors = require('colors')
+
+console.clear()
+
+console.log("Hello World!".brightMagenta.bgYellow.bold)
--- Fundamentos/sintaxe1.js
+++ Fundamentos/sintaxe1.js
@@ -15,23 +15,33 @@ let idade = 19
 let altura = 1.90
 // typeof() é usado para identificar o tipo de
 // variável
+console.log("____________")
+console.log("Typeof(nome):")
 console.log(typeof(nome))
 console.log(typeof(idade))
 console.log(typeof(altura))
 
 // identificando variáveis inteiras e não inteiras
+console.log("_________________________________")
+console.log("Variáveis inteiras e não inteiras:")
 console.log(Number.isInteger(idade))
 console.log(Number.isInteger(altura))
 
 // concatenação (unir textos e variáveis)
+console.log("_____________________________________")
+console.log("Concatenação (unir textos e variáveis:")
 console.log(`Nome: ${nome}`)
 console.log("Nome: " + nome) // não recomendado
 
 // arredondamento (casas decimais)
+console.log("_______________________________")
+console.log("Arredondamento (casas decimais):")
 let media = 5.789
 console.log(`Média: ${media.toFixed(1)}`)
 
 // Particularidades do JavaScript
+console.log("________________")
+console.log("Particularidades:")
 console.log(10 / 0)
 console.log(0.5 + 0.2)
 console.log(0.1 + 0.2)
