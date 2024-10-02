2914aabb9b7bcd26431963bddc217b3ab5195c22
CLAUDIO HENRIQUE ROCHA DA SILVA <claudio.hrsilva@senacsp.edu.br>
2024-10-01 22:19:05 -0300

    Exercícios

---
+++ Exercícios/desconto.js
@@ -0,0 +1,8 @@
+/**
+ * Cálculo de desconto em uma compra:
+ Desenvolva um programa que calcule o valor final
+ de um produto após um desconto. O usuário deve
+ inserir o preço original e o percentual de desconto.
+ O programa deve calcular e exibir o valor final.
+ @author CLaudio Silva
+ */
