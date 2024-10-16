3dfcad8ec0bbf816af43deb1ca1fc6bc8849dc78
CLAUDIO HENRIQUE ROCHA DA SILVA <claudio.hrsilva@senacsp.edu.br>
2024-10-15 22:19:02 -0300

    IMC ELSE IF

--- Exercícios/academia.js
+++ Exercícios/academia.js
@@ -18,14 +18,14 @@ console.clear()
 nome  = "Dean Winchester"
 idade = 24
 altura = 1.80
-peso = 72
+peso = 54
 vip = true
 
 //processamento;
 fcm = 208 - (0.7 * idade)
 //imc contém 3 variáveis, sendo eles:
 //peso,altura, imc
-imc = 72 / (1.80 * 1.80)
+imc = peso / (1.80 * 1.80)
 
 
 // saida
@@ -40,3 +40,17 @@ console.log(`FCM: ${fcm}`)
 console.log(`IMC: ${imc.toFixed(2)}`) //".toFixed" formata em casas
 //decimais de acordo com o seu cenário. Ex:
 //.tiFixed(2)   "2" é a formatação
+// tabela imc
+if (imc < 18.5) {
+    console.log("Abaixo do peso")
+} else if (imc < 25) {
+    console.log("Peso normal")
+} else if (imc < 30) {
+    console.log("Levemente acima do peso")
+} else if (imc < 35) {
+    console.log("Obesidade Grau I")
+} else if (imc < 40) {
+    console.log("Obesidade grau II (severa)")
+} else {
+    console.log("Obesidade grau III (mórbida)")
+}
