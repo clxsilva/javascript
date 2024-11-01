852a9ec9d897bb80d764025a86768e0e25977711
CLAUDIO HENRIQUE ROCHA DA SILVA <claudio.hrsilva@senacsp.edu.br>
2024-11-01 19:24:13 -0300

    adicional

--- Exercícios/calculadora.js
+++ Exercícios/calculadora.js
@@ -27,45 +27,42 @@ console.log("5. Calcular a porcentagem")
 let calculadora = Number(input.question("Digite a opcao desejada: "))
 
 switch (calculadora) {
-    case 1:
-        console.log("Somar")
-        function somar (a, b) {
-            return console.log(`A soma de ${a} + ${b} = ${a + b}`) 
-        }      
+    case 1:   
         somar(a, b)
         break
     case 2:
-        console.log("Subtrair")
-        function subtrair (a, b) {
-            return console.log(`A subtração de ${a} - ${b} = ${a - b}`)
-        }
         subtrair(a, b)
         break
     case 3:
-        console.log("Multiplicar")
-        function multiplicar (a, b) {
-            return console.log(`A multiplicação de ${a} x ${b} = ${a * b}`)
-        }
         multiplicar(a, b)
         break
     case 4:
-        console.log("Dividir")
-        function dividir (a, b) {
-            return console.log(`A divisão de ${a} / ${b} = ${a / b}`)
-        }
         dividir(a, b)
         break
     case 5:
-        console.log("Porcentagem")
-        function porcentagem (a, b) {
-            return console.log(`A porcentagem de ${a}% ${b} é igual a: ${(a * b) / 100}`)
-        }
         porcentagem(a, b)
         break
     default:
         console.log("Opção não válida")
         break
 }
+
 console.log("")
 opcao = input.question("Deseja jogar novamente? (s/n) ")
 } while (opcao === 's');
+
+function somar (a, b) {
+    return console.log(`A soma de ${a} + ${b} = ${a + b}`) 
+}
+function subtrair (a, b) {
+    return console.log(`A subtração de ${a} - ${b} = ${a - b}`)
+}
+function multiplicar (a, b) {
+    return console.log(`A multiplicação de ${a} x ${b} = ${a * b}`)
+}
+function dividir (a, b) {
+    return console.log(`A divisão de ${a} / ${b} = ${a / b}`)
+} 
+function porcentagem (a, b) {
+    return console.log(`A porcentagem de ${a}% ${b} é igual a: ${(a * b) / 100}`)
+}
---
+++ htmljs/script.js
@@ -0,0 +1,10 @@
+/**
+ * Hello JS
+ * Exemplo de ligação do documento HTMl com o JavaScript
+ * @author Claudio Silva
+ */
+
+function helloJS() {
+    let nome = prompt("Digite o seu nome: ")
+    alert(`Hello ${nome}`)
+}
